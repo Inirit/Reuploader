c380348af7e009fdb52b4a190802259dfb0647f4
Inirit <gbbrinzea@gmail.com>
2017-11-17 03:34:39 -0800

    Way better code organization, url is parsed from upload result and written to clipboard. It actually works.

--- js/background.js
+++ js/background.js
@@ -1,75 +1,49 @@
 System.register(["jquery"], function (exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    function onCreated() {
+    function OnCreated() {
         if (browser.runtime.lastError) {
-            console.log(`Error: ${browser.runtime.lastError}`);
+            console.error(`Error: ${browser.runtime.lastError}`);
         }
         else {
-            console.log("Item created successfully");
+            console.debug("Item created successfully");
         }
     }
-    function onRemoved() {
-        console.log("Item removed successfully");
+    function DoSomethingWithNewUrl(uploadedUrl) {
+        console.debug(`UploadedUrl: ${uploadedUrl}`);
+        const input = document.createElement('input');
+        input.style.position = 'fixed';
+        input.style.opacity = '0';
+        input.value = uploadedUrl;
+        document.body.appendChild(input);
+        input.select();
+        document.execCommand('Copy');
+        document.body.removeChild(input);
     }
-    function onError(error) {
-        console.log(`Error: ${error}`);
-    }
-    function uploadImage(imageData) {
-        let formData = new FormData();
-        formData.append('files[]', imageData, "image.jpg");
-        console.log(formData.get('files[]'));
-        let jqXHR = jquery_1.default.ajax({
-            url: 'https://pomf.cat/upload.php',
-            type: 'POST',
-            data: formData,
-            cache: false,
-            contentType: false,
-            processData: false,
-            xhr: function () {
-                var myXhr = jquery_1.default.ajaxSettings.xhr();
-                if (myXhr.upload) {
-                    // For handling the progress of the upload
-                    myXhr.upload.addEventListener('progress', function (e) {
-                        if (e.lengthComputable) {
-                            jquery_1.default('progress').attr({
-                                value: e.loaded,
-                                max: e.total,
-                            });
-                            console.log(`Progress: ${e.loaded}, ${e.total}`);
-                        }
-                    }, false);
-                }
-                return myXhr;
+    function Initialize() {
+        console.debug(`Initializing...`);
+        browser.contextMenus.create({
+            id: "reuploadImageMenuItem",
+            title: browser.i18n.getMessage("reuploadImageMenuItemLabel"),
+            contexts: ["image"]
+        }, OnCreated);
+        browser.contextMenus.onClicked.addListener((info, tab) => {
+            switch (info.menuItemId) {
+                case "reuploadImageMenuItem":
+                    if (!info.srcUrl) {
+                        console.error(`Image was selected, but src url could not be found!`);
+                    }
+                    else {
+                        console.debug(`Image selected.SrcUrl: ${info.srcUrl}, TabId: ${tab.id}`);
+                        let handler = new PomfHandler();
+                        handler.FetchImage(info.srcUrl, (image) => handler.UploadImage(image, DoSomethingWithNewUrl));
+                    }
+                    break;
             }
-        })
-            .done(data => {
-            console.log("Image upload complete!");
-            console.log(data);
-        })
-            .fail((jqXHR, textStatus, error) => {
-            console.error(`Image upload failed! Status: ${textStatus}, Error: ${error}`);
         });
+        console.debug(`Initialization complete.`);
     }
-    function getImage(url) {
-        jquery_1.default.ajax({
-            url: url,
-            type: "GET",
-            xhr: function () {
-                var myXhr = jquery_1.default.ajaxSettings.xhr();
-                myXhr.responseType = "blob";
-                return myXhr;
-            }
-        })
-            .done((data) => {
-            console.debug(`Image download complete. Size: ${data.size}, Type: ${data.type}`);
-            uploadImage(data);
-        })
-            .fail((jqXHR, textStatus, error) => {
-            console.error(`Image download failed! Status: ${textStatus}, Error: ${error}`);
-        });
-    }
-    var jquery_1;
+    var jquery_1, HandlerType, HandlerBase, PomfHandler;
     return {
         setters: [
             function (jquery_1_1) {
@@ -77,24 +51,73 @@ System.register(["jquery"], function (exports_1, context_1) {
             }
         ],
         execute: function () {
-            browser.contextMenus.create({
-                id: "reuploadImageMenuItem",
-                title: browser.i18n.getMessage("reuploadImageMenuItemLabel"),
-                contexts: ["image"]
-            }, onCreated);
-            browser.contextMenus.onClicked.addListener((info, tab) => {
-                switch (info.menuItemId) {
-                    case "reuploadImageMenuItem":
-                        if (!info.srcUrl) {
-                            console.error(`Image was selected, but src url could not be found!`);
+            (function (HandlerType) {
+                HandlerType[HandlerType["Pomf"] = 0] = "Pomf";
+                HandlerType[HandlerType["Imgur"] = 1] = "Imgur";
+            })(HandlerType || (HandlerType = {}));
+            HandlerBase = class HandlerBase {
+                FetchImage(url, callback) {
+                    jquery_1.default.ajax({
+                        url: url,
+                        type: "GET",
+                        xhr: function () {
+                            var myXhr = jquery_1.default.ajaxSettings.xhr();
+                            myXhr.responseType = "blob";
+                            return myXhr;
                         }
-                        else {
-                            console.debug(`Image selected. SrcUrl: ${info.srcUrl}, TabId: ${tab.id}`);
-                            getImage(info.srcUrl);
-                        }
-                        break;
+                    })
+                        .done((data) => {
+                        console.debug(`Image download complete. Size: ${data.size}, Type: ${data.type}`);
+                        callback(data);
+                    })
+                        .fail((jqXHR, textStatus, error) => {
+                        console.error(`Image download failed! Status: ${textStatus}, Error: ${error}`);
+                    });
                 }
-            });
+            };
+            PomfHandler = class PomfHandler extends HandlerBase {
+                constructor() {
+                    super(...arguments);
+                    this._uploadUrl = 'https://pomf.cat/upload.php';
+                    this._uploadPrefix = 'https://a.pomf.cat/';
+                }
+                get HandlerType() {
+                    return HandlerType.Pomf;
+                }
+                UploadImage(image, callback) {
+                    let formData = new FormData();
+                    formData.append('files[]', image, "image.jpg");
+                    let jqXHR = jquery_1.default.ajax({
+                        url: this._uploadUrl,
+                        type: 'POST',
+                        data: formData,
+                        cache: false,
+                        contentType: false,
+                        processData: false,
+                        xhr: function () {
+                            var myXhr = jquery_1.default.ajaxSettings.xhr();
+                            if (myXhr.upload) {
+                                myXhr.upload.addEventListener('progress', function (e) {
+                                    if (e.lengthComputable) {
+                                        console.debug(`Progress: ${e.loaded}, ${e.total}`);
+                                    }
+                                }, false);
+                            }
+                            return myXhr;
+                        }
+                    })
+                        .done(data => {
+                        console.debug("Image upload complete!");
+                        let uploadedFile = data.files[0];
+                        let uploadedFileName = uploadedFile.url;
+                        callback(`${this._uploadPrefix}${uploadedFileName}`);
+                    })
+                        .fail((jqXHR, textStatus, error) => {
+                        console.error(`Image upload failed! Status: ${textStatus}, Error: ${error}`);
+                    });
+                }
+            };
+            Initialize();
         }
     };
 });
--- ts/background.ts
+++ ts/background.ts
@@ -1,121 +1,167 @@
 import $ from 'jquery';
 
-function onCreated()
+enum HandlerType
+{
+	Pomf,
+	Imgur
+}
+
+interface IHandler
+{
+	HandlerType: HandlerType;
+
+	UploadImage(image: Blob, callback: (uploadedUrl: string) => void): void;
+
+	FetchImage(url: string, callback: (image: Blob) => void): void;
+}
+
+abstract class HandlerBase implements IHandler
+{
+	abstract HandlerType: HandlerType;
+	abstract UploadImage(image: Blob, callback: (uploadedUrl: string) => void): void;
+
+	FetchImage(url: string, callback: (image: Blob) => void): void
+	{
+		$.ajax(
+			{
+				url: url,
+				type: "GET",
+				xhr: function ()
+				{
+					var myXhr = $.ajaxSettings.xhr();
+					myXhr.responseType = "blob";
+					return myXhr;
+				}
+			})
+			.done((data: Blob) =>
+			{
+				console.debug(`Image download complete. Size: ${data.size}, Type: ${data.type}`);
+
+				callback(data);
+			})
+			.fail((jqXHR, textStatus, error) =>
+			{
+				console.error(`Image download failed! Status: ${textStatus}, Error: ${error}`);
+			});
+	}
+}
+
+class PomfHandler extends HandlerBase
+{
+	private readonly _uploadUrl: string = 'https://pomf.cat/upload.php';
+	private readonly _uploadPrefix: string = 'https://a.pomf.cat/';
+
+	get HandlerType(): HandlerType
+	{
+		return HandlerType.Pomf;
+	}
+
+	public UploadImage(image: Blob, callback: (uploadedUrl: string) => void): void
+	{
+		let formData = new FormData();
+		formData.append('files[]', image, "image.jpg");
+
+		let jqXHR = $.ajax(
+			{
+				url: this._uploadUrl,
+				type: 'POST',
+				data: formData,
+				cache: false,
+				contentType: false,
+				processData: false,
+				xhr: function ()
+				{
+					var myXhr = $.ajaxSettings.xhr();
+					if (myXhr.upload)
+					{
+						myXhr.upload.addEventListener('progress', function (e)
+						{
+							if (e.lengthComputable)
+							{
+								console.debug(`Progress: ${e.loaded}, ${e.total}`);
+							}
+						}, false);
+					}
+					return myXhr;
+				}
+			})
+			.done(data =>
+			{
+				console.debug("Image upload complete!");
+
+				let uploadedFile = data.files[0]
+				let uploadedFileName = uploadedFile.url;
+
+				callback(`${this._uploadPrefix}${uploadedFileName}`);
+			})
+			.fail((jqXHR, textStatus, error) =>
+			{
+				console.error(`Image upload failed! Status: ${textStatus}, Error: ${error}`);
+			});
+	}
+}
+
+function OnCreated()
 {
 	if (browser.runtime.lastError)
 	{
-		console.log(`Error: ${browser.runtime.lastError}`);
+		console.error(`Error: ${browser.runtime.lastError}`);
 	} else
 	{
-		console.log("Item created successfully");
+		console.debug("Item created successfully");
 	}
 }
 
-function onRemoved()
+function DoSomethingWithNewUrl(uploadedUrl: string)
 {
-	console.log("Item removed successfully");
+	console.debug(`UploadedUrl: ${uploadedUrl}`);
+
+	const input = document.createElement('input');
+	input.style.position = 'fixed';
+	input.style.opacity = '0';
+	input.value = uploadedUrl;
+	document.body.appendChild(input);
+	input.select();
+	document.execCommand('Copy');
+	document.body.removeChild(input);
 }
 
-function onError(error: string)
+function Initialize()
 {
-	console.log(`Error: ${error}`);
-}
+	console.debug(`Initializing...`);
 
-function uploadImage(imageData: Blob)
-{
-	let formData = new FormData();
-	formData.append('files[]', imageData, "image.jpg");
-
-	console.log(formData.get('files[]'));
-
-	let jqXHR = $.ajax(
+	browser.contextMenus.create(
 		{
-			url: 'https://pomf.cat/upload.php',
-			type: 'POST',
-			data: formData,
-			cache: false,
-			contentType: false,
-			processData: false,
-			xhr: function ()
-			{
-				var myXhr = $.ajaxSettings.xhr();
-				if (myXhr.upload)
-				{
-					// For handling the progress of the upload
-					myXhr.upload.addEventListener('progress', function (e)
-					{
-						if (e.lengthComputable)
-						{
-							$('progress').attr({
-								value: e.loaded,
-								max: e.total,
-							});
+			id: "reuploadImageMenuItem",
+			title: browser.i18n.getMessage("reuploadImageMenuItemLabel"),
+			contexts: ["image"]
+		}, OnCreated);
 
-							console.log(`Progress: ${e.loaded}, ${e.total}`);
-						}
-					}, false);
-				}
-				return myXhr;
-			}
-		})
-		.done(data =>
-		{
-			console.log("Image upload complete!");
-			console.log(data);
-		})
-		.fail((jqXHR, textStatus, error) =>
-		{
-			console.error(`Image upload failed! Status: ${textStatus}, Error: ${error}`);
-		});
-}
-
-function getImage(url: string)
-{
-	$.ajax(
-		{
-			url: url,
-			type: "GET",
-			xhr: function ()
-			{
-				var myXhr = $.ajaxSettings.xhr();
-				myXhr.responseType = "blob";
-				return myXhr;
-			}
-		})
-		.done((data: Blob) =>
-		{
-			console.debug(`Image download complete. Size: ${data.size}, Type: ${data.type}`);
-
-			uploadImage(data);
-		})
-		.fail((jqXHR, textStatus, error) =>
-		{
-			console.error(`Image download failed! Status: ${textStatus}, Error: ${error}`);
-		});
-}
-
-browser.contextMenus.create({
-	id: "reuploadImageMenuItem",
-	title: browser.i18n.getMessage("reuploadImageMenuItemLabel"),
-	contexts: ["image"]
-}, onCreated);
-
-browser.contextMenus.onClicked.addListener((info, tab) =>
-{
-	switch (info.menuItemId)
+	browser.contextMenus.onClicked.addListener((info, tab) =>
 	{
-		case "reuploadImageMenuItem":
-			if (!info.srcUrl)
-			{
-				console.error(`Image was selected, but src url could not be found!`);
-			}
-			else
-			{
-				console.debug(`Image selected. SrcUrl: ${info.srcUrl}, TabId: ${tab.id}`);
+		switch (info.menuItemId)
+		{
+			case "reuploadImageMenuItem":
+				if (!info.srcUrl)
+				{
+					console.error(`Image was selected, but src url could not be found!`);
+				}
+				else
+				{
+					console.debug(`Image selected.SrcUrl: ${info.srcUrl}, TabId: ${tab.id}`);
 
-				getImage(info.srcUrl);
-			}
+					let handler = new PomfHandler();
 
-			break;
-	}
-});
+					handler.FetchImage(
+						info.srcUrl,
+						(image: Blob) => handler.UploadImage(image, DoSomethingWithNewUrl));
+				}
+
+				break;
+		}
+	});
+
+	console.debug(`Initialization complete.`);
+}
+
+Initialize();
